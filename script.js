7d8bb609976c86d47ef7ff8e7579a71e28ca69fa
gescofet09 <gabriela.escofet09@gmail.com>
2026-01-15 09:35:51 +0000

    carb

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-document.getElementById('boton').addEventListener('click', function runHeavy() {
-    var result = 0;
-    for (var i = 0; i < 1000; i++) {
-        result += Math.sqrt(i) * Math.sin(i);
-    }
-    alert('Proceso terminado: ' + result);
-})
-
 var list = document.getElementById('list');
 for (var i = 0; i < 100; i++) {
     var li = document.createElement('li');
