5195e19d31b108368c3eac6d200b0e52109ad732
gescofet09 <gabriela.escofet09@gmail.com>
2026-01-29 08:42:19 +0000

    Cambios 29/01

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-for (let i = 0; i < 100; i++) {
-  const li = document.createElement('li');
-  li.textContent = 'Elemento ' + i;
-  frag.appendChild(li);
-}
+const boton = document.getElementById('boton');
+const list = document.getElementById('list');
+
+boton.addEventListener('click', () => {
+  const frag = document.createDocumentFragment();
+
+  for (let i = 0; i < 100; i++) {
+    const li = document.createElement('li');
+    li.textContent = 'Elemento ' + i;
+    frag.appendChild(li);
+  }
+
+  list.appendChild(frag);
+});
